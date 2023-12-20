04a16124e1f38fc4663d19f9bc09ef1c8e0eef54
Orlando Landaeta <63929875+landaetadev@users.noreply.github.com>
2023-12-20 02:17:16 +0100

    JSON index.js
    update files js and JSON
    2 more apps

--- js/index.js
+++ js/index.js
@@ -22,31 +22,32 @@ function ReadTable(JSONData) {
 		vMakeMenuHTML += '<li>'
         if (vJSONi == 0){
             vMakeMenuHTML += `<svg class="icoLogo" id="icoLogoApple">`
-            vMakeMenuHTML += `<use href="./assets/images/icons.svg#${JSONData[vJSONi].icoSeccion}">`
+            vMakeMenuHTML +=    `<use href="./assets/images/icons.svg#${JSONData[vJSONi].icoSeccion}">`
             vMakeMenuHTML += `</svg>`
         }else{
             vMakeMenuHTML += `<a href="${JSONData[vJSONi].linkSeccion}" title="${JSONData[vJSONi].txtSeccion}">`
-            vMakeMenuHTML += `<svg class="icoLogo">`
-            vMakeMenuHTML += `<use href="./assets/images/icons.svg#${JSONData[vJSONi].icoSeccion}">`
-            vMakeMenuHTML += `</svg> ${JSONData[vJSONi].txtSeccion}`
+            vMakeMenuHTML +=    `<svg class="icoLogo">`
+            vMakeMenuHTML +=        `<use href="./assets/images/icons.svg#${JSONData[vJSONi].icoSeccion}">`
+            vMakeMenuHTML +=    `</svg> ${JSONData[vJSONi].txtSeccion}`
             vMakeMenuHTML += `</a>`
         }
         vMakeMenuHTML += '</li>'
 
         if (JSONData[vJSONi].apps.length > 0) {
             vMakeAppsHTML += `<div class="classSectionApps">`
-            vMakeAppsHTML += `<h3 id="id${JSONData[vJSONi].txtSeccion}">${JSONData[vJSONi].txtSeccion}</h3>`
+            vMakeAppsHTML +=    `<h3 id="id${JSONData[vJSONi].txtSeccion}">${JSONData[vJSONi].txtSeccion}</h3>`
             vMakeAppsHTML += `<div class="classApps">`
 
+            //Ordena alfabeticamente las apps de cada seccion del JSON
+            JSONData[vJSONi].apps.sort((a,b) => a.nombreApp.localeCompare(b.nombreApp));
+
             for (var vNumApp = 0; vNumApp < JSONData[vJSONi].apps.length; vNumApp++) {
-                
-                vMakeAppsHTML +=         `<div class="classApp">`
-                vMakeAppsHTML +=             `<a href="${JSONData[vJSONi].apps[vNumApp].linkApp}" target="_blank">`
-                vMakeAppsHTML +=                `<img src="${vFolderLogosApps}${JSONData[vJSONi].apps[vNumApp].logoApp}" alt="${JSONData[vJSONi].apps[vNumApp].nombreApp}">`
-                vMakeAppsHTML +=                 `<h4>${JSONData[vJSONi].apps[vNumApp].nombreApp}</h4>`
-                vMakeAppsHTML +=             `</a>`
-                vMakeAppsHTML +=         `</div>`
-                
+                vMakeAppsHTML += `<div class="classApp">`
+                vMakeAppsHTML +=    `<a href="${JSONData[vJSONi].apps[vNumApp].linkApp}" target="_blank">`
+                vMakeAppsHTML +=        `<img src="${vFolderLogosApps}${JSONData[vJSONi].apps[vNumApp].logoApp}" alt="${JSONData[vJSONi].apps[vNumApp].nombreApp}">`
+                vMakeAppsHTML +=        `<h4>${JSONData[vJSONi].apps[vNumApp].nombreApp}</h4>`
+                vMakeAppsHTML +=    `</a>`
+                vMakeAppsHTML += `</div>`
             }
             vMakeAppsHTML += `</div>`
             vMakeAppsHTML += `</div>`
